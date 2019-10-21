26600cf8b7fc58f0ec1400fbc80c7ac5fe42767a
freeswoo <freeswoo@naver.com>
2019-10-21 16:42:41 +0900

    2019-10-21

---
+++ JDBC_04_Oracle/src/com/biz/oracle/exec/BookNameEx_01.java
@@ -0,0 +1,19 @@
+package com.biz.oracle.exec;
+
+import com.biz.oracle.service.BookServiceV1;
+
+public class BookNameEx_01 {
+
+	public static void main(String[] args) {
+
+		BookServiceV1 bs = new BookServiceV1();
+		
+		// searchBookName() method에
+		// true값을 파라메터로 전달을 하면
+		// 반복하면서 도서이름
+		bs.searchBookName(true);
+		System.out.println("도서검색 종료");
+		
+	}
+
+}
